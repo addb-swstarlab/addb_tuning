3e1f546a37e54fd000254a11534472041b010da2
sein <tpdlskwon@naver.com>
2022-09-16 17:33:23 +0900

    update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import datetime
 import os, logging
+import pandas as pd
 
 def get_filename(PATH, head, tail):
     i = 0
@@ -35,3 +36,19 @@ def get_logger(log_path='./logs'):
     logger.setLevel(logging.INFO)
     logger.info('Writing logs at {}'.format(os.path.join(log_path, name)))
     return logger, os.path.join(log_path, name)
+
+def save_results(addb, results):
+    redis_bool_knobs = ['lazyfree-lazy-expire', 'lazyfree-lazy-server-del', 'lazyfree-lazy-eviction', 'activerehashing']
+    spark_bool_knobs = ['spark.broadcast.compress', 'spark.rdd.compress', 'spark.shuffle.compress', 'spark.shuffle.spill.compress',
+                        'spark.sql.codegen.aggregate.map.twolevel.enable', 'spark.sql.inMemoryColumnarStorage.compressed',
+                        'spark.sql.inMemoryColumnarStorage.partitionPruning', 'spark.sql.join.preferSortMergeJoin',
+                        'spark.sql.sort.enableRadixSort']
+    
+    pd_redis = pd.DataFrame(data=[results[:addb.redis_len].astype(int)], columns=addb.redis_knobs_columns)
+    pd_rocksdb = pd.DataFrame(data=[results[addb.redis_len:addb.redis_len+addb.rocksdb_len].astype(int)], columns=addb.rocksdb_knobs_columns)
+    pd_spark = pd.DataFrame(data=[results[-addb.spark_len:].astype(int)], columns=addb.spark_knobs_columns)
+    
+    if not os.path.exists('results'):
+        os.mkdir('results')
+    
+    
