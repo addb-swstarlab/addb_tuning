4991856ead669a5605721167e846b147d1e5eeb0
DELAB <48348166+addb-swstarlab@users.noreply.github.com>
2023-10-25 21:37:39 +0900

    Update gcp_info.py

--- models/gcp_info.py
+++ models/gcp_info.py
@@ -1,2 +1,3 @@
 GCP_SPARK_MASTER_ADDRESS = 'user@remote_ip' # insert your remote ip and user name
 GCP_INSTANCE_PASSWD = 'passwd' # insert your remote user password
+GCP_SPARK_CONF_PATH = '/home/user/addb-spark/test_spark.conf' # insert your remote path of spark configuration
