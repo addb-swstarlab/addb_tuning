e87d1ba946d2b06add3b81b40d7c1c75d26879ca
dkdl012 <31770302+dkdl012@users.noreply.github.com>
2023-11-15 20:49:32 +0900

    Remove redundant vairables and add new one

--- models/configs.py
+++ models/configs.py
@@ -15,20 +15,5 @@ TPCH_RESULT_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'envs/result.txt')
 
 INCUMBENTS_RESULTS_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'results')
 
-baxus_params = {'embedding_type': 'baxus',
-                'acquisition_function': 'ts',
-                'mle_optimization': 'sample-and-choose-best',
-                'target_dim': 4, # 10
-                'n_init': 10, # 10
-                'max_evals': 20, # 100
-                'noise_std': 0,
-                'new_bins_on_split': 2,
-                'multistart_samples': 100,
-                'mle_training_steps': 50,
-                'multistart_after_samples': 10,
-                'l_init': 0.8,
-                'l_min': 0.5**7,
-                'l_max': 1.6,
-                'adjust_initial_target_dim': True,
-                'budget_until_input_dim': 0
-                }
+HISTORY_DATA_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'history_data.csv')
+SAVE_HISTORY_FOLDER_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'history')
