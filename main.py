8d7fc84b715f82bcf655286070326bb79c91f8e4
dkdl012 <31770302+dkdl012@users.noreply.github.com>
2023-11-15 20:45:39 +0900

    Modify main.py to other BO

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import os
-import pandas as pd
 import numpy as np
 import argparse
-
-import baxus
-from baxus.util.behaviors import BaxusBehavior
-from baxus.util.behaviors.gp_configuration import GPBehaviour
-from baxus.baxus import BAxUS
-from baxus.util.parsing import embedding_type_mapper, acquisition_function_mapper, mle_optimization_mapper
 
 from envs.spark import SparkParameters
 from models.utils import get_logger, get_foldername, get_filename
 from models.bench import SparkBench
 from models.configs import baxus_params as bp
 import models.configs as cfg
+from models.bo import BO_Tuner
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--bo', type=str, default='baxus')
+parser.add_argument('-b', '--bo', type=str, default='vanilla')
 parser.add_argument('-q', '--query', type=int, default=1, help='Define a number of tpc-h query to test')
+parser.add_argument('-t', '--trials', type=int, default=1, help='Define a number of iteration to tune')
 
 logger = get_logger()
 os.system('clear')
@@ -29,54 +24,27 @@ for _ in vars(opt):
 os.makedirs(cfg.INCUMBENTS_RESULTS_PATH, exist_ok=True)
 
 def main():
-    bin_sizing_method = embedding_type_mapper[bp['embedding_type']]
-    acquisition_function = acquisition_function_mapper[bp['acquisition_function']]
-    mle_optimization_method = mle_optimization_mapper[bp['mle_optimization']]
-
-    behavior = BaxusBehavior(n_new_bins=bp['new_bins_on_split'],
-                            initial_base_length=bp['l_init'],
-                            min_base_length=bp['l_min'],
-                            max_base_length=bp['l_max'],
-                            acquisition_function=acquisition_function,
-                            embedding_type=bin_sizing_method,
-                            adjust_initial_target_dim=bp['adjust_initial_target_dim'],
-                            noise=bp['noise_std'],
-                            budget_until_input_dim=bp['budget_until_input_dim']
-                            )
-
-    gp_behaviour = GPBehaviour(
-        mll_estimation=mle_optimization_method,
-        n_initial_samples=bp['multistart_samples'],
-        n_best_on_lhs_selection=bp['multistart_after_samples'],
-        n_mle_training_steps=bp['mle_training_steps'],
-    )
-
     sp = SparkParameters()
+    
+    sb = SparkBench(sp=sp, bench_type=opt.query, history_dir=get_foldername('history'))
+    
+    bo_tuner = BO_Tuner(sb=sb,
+                        trg_wk=opt.query,
+                        history_data_path=cfg.HISTORY_DATA_PATH)
+    
+    for trial in range(1, opt.trials):
+        logger.info(f"\n####Trials {trial:>2} of {opt.trials} ####")
+        
+        bo_tuner.optimize()
 
-    input_dim = len(sp) # args.input_dim
-    ub = sp.ub
-    lb = sp.lb
+    INC_C_PATH = os.path.join(cfg.INCUMBENTS_RESULTS_PATH, get_filename(cfg.INCUMBENTS_RESULTS_PATH,'tuned_configs', '.npy'))
+    INC_R_PATH = os.path.join(cfg.INCUMBENTS_RESULTS_PATH, get_filename(cfg.INCUMBENTS_RESULTS_PATH,'tuned_results', '.npy'))
+    np.save(INC_C_PATH, bo_tuner.best_observed_configs_all)
+    np.save(INC_R_PATH, bo_tuner.best_observed_res_all)
+    logger.info("save incumbent at..")
+    logger.info(f"configurations : {INC_C_PATH}")
+    logger.info(f"results        : {INC_R_PATH}")
 
-    f = SparkBench(dim=input_dim, ub=ub, lb=lb, sp=sp, bench_type=opt.query, history_dir=get_foldername('history'))
-    
-    optim = BAxUS(f=f,
-                n_init=bp['n_init'],
-                max_evals=bp['max_evals'],
-                target_dim=bp['target_dim'],
-                behavior=behavior,
-                gp_behaviour=gp_behaviour,
-                run_dir=get_foldername(os.path.join('bo-results', opt.bo))
-                )
-
-    optim.optimize()
-    
-    incumbent_configs, incumbent_results = optim.optimization_results_incumbent()
-    np.save(get_filename(cfg.INCUMBENTS_RESULTS_PATH,'tuned_configs', '.npy'), incumbent_configs)
-    np.save(get_filename(cfg.INCUMBENTS_RESULTS_PATH,'tuned_results', '.npy'), incumbent_results)
-    
-    # np.save(os.path.join(cfg.INCUMBENTS_RESULTS_PATH, 'tuned_configs.npy'), incumbent_configs)
-    # np.save(os.path.join(cfg.INCUMBENTS_RESULTS_PATH, 'tuned_results.npy'), incumbent_results)
-    
 
 if __name__ == '__main__':
     try:
