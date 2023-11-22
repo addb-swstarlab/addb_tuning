cdb54610160ab85d7f3ab4a2d2f03680ed45c6fe
dkdl012 <31770302+dkdl012@users.noreply.github.com>
2023-11-22 14:48:30 +0900

    update for processing sql files

--- models/bench.py
+++ models/bench.py
@@ -1,15 +1,48 @@
 import os, logging
 import subprocess
+import pandas as pd
 import numpy as np
-from models.configs import *
+import models.configs as cfg
+from envs.spark import SparkParameters
+from envs.query_featurization import QueryFeatureGenerator
 
 class SparkBench:
-    def __init__(self, sp, bench_type=None, history_dir=None):
+    def __init__(self, sp:SparkParameters, bench_type:int=None, sql_path:str=None):
         self.dim = len(sp)
         self.ub = sp.ub
         self.lb = sp.lb
         self.sp = sp # spark parameter infos
         self.bench_type = bench_type
+        self.sql_path = sql_path
+        self.parameter_names = sp.parameter_names
+
+        assert (self.bench_type is not None and self.sql_path is None) or (self.bench_type is None and self.sql_path is not None), "Please use only one option, bench_type or sql_path"
+
+        self._set_query_features()
+
+    def __len__(self):
+        return self.dim
+
+    def _set_query_features(self):
+        self.q_features = pd.read_csv(cfg.QUERY_FEATURE_TABLE_PATH)
+        cfg.set_query_feature_name(list(self.q_features.columns))
+        self.q_len = len(self.q_features.columns)
+        if self.sql_path is not None:
+            self.q_feat_gen = QueryFeatureGenerator(self.sql_path)
+
+    def get_workload_feature(self, bench_type:int=None, sql_path:str=None) -> list:
+        if bench_type is None:
+            bench_type = self.bench_type
+        if sql_path is None:
+            sql_path = self.sql_path
+        assert (bench_type is not None and sql_path is None) or (bench_type is None and sql_path is not None), "Please use only one option, bench_type or sql_path"
+        
+        if bench_type is not None:
+            b_feature = self.q_features.iloc[int(bench_type)-1].values
+        else:
+            b_feature = self.q_feat_gen.get_query_vector()
+        
+        return list(b_feature)
     
     def benchmark(self, x:np.array) -> float:
         spark_conf_dict = self.sp.save_configuration_file(x)
