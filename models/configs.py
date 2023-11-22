e142a7a7bf15ea52c94f3788687de4e2b101d581
dkdl012 <31770302+dkdl012@users.noreply.github.com>
2023-11-22 14:42:48 +0900

    add new variables

--- models/configs.py
+++ models/configs.py
@@ -15,5 +15,11 @@ TPCH_RESULT_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'envs/result.txt')
 
 INCUMBENTS_RESULTS_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'results')
 
-HISTORY_DATA_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'history_data.csv')
+HISTORY_DATA_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'history_feature_data.csv')
 SAVE_HISTORY_FOLDER_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'history')
+
+QUERY_FEATURE_TABLE_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'query_tables.csv')
+
+def set_query_feature_name(cols):
+    global QUERY_FEATURE_NAMES
+    QUERY_FEATURE_NAMES = cols
